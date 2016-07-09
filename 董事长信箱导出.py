de32dfa75e74163c0e1c812bad2cdc037793dcf0
gslbusiness <guosiliang66@qq.com>
2016-07-09 22:42:49 +0800

    Update 董事长信箱导出.py
    1、参考数据挖掘教程，更改生成初始数据框的代码
    2、增加判断已导出数据与需导出数据的差额，只导出未导内容

--- 董事长信箱导出.py
+++ 董事长信箱导出.py
@@ -1,12 +1,22 @@
-from multiprocessing import Pool
 from bs4 import BeautifulSoup
 import requests
 from selenium import webdriver
 import time
 import pandas as pd
 
+#统计已获取列表
 
-def get_info(id):
+#df_get=pd.DataFrame(columns=['标题','来信日期','回复单位','回复日期','来信内容','回复内容','link','id'])
+df_get=pd.read_csv(path+"/董事长信箱1.csv", encoding='gbk')
+path='C:/Users/Administrator/Desktop/董事长信箱解答/董事长信箱记录导出'
+df_previous=pd.read_csv(path+"/董事长信箱.csv", encoding='gbk')
+df_id=df_previous[~df_previous['id'].isin(df_get['id'])]
+driver = webdriver.PhantomJS(executable_path=path+"/phantomjs-2.1.1-windows/bin/phantomjs.exe")
+
+df=df_id['id']
+for i in df.index:
+    id=df[i]
+    print(i,id)
     url='http://192.168.1.235:7001/default/com.dszxx.portal.xxb.LetterDetial.flow?id={}'.format(id)
     wb_data=requests.get(url)
     soup=BeautifulSoup(wb_data.text,'lxml')
@@ -26,51 +36,6 @@ def get_info(id):
         except:
             replycontent=""
             print("无回复内容")
-        data={
-        '标题':[title,0],
-        '来信日期':[date,0],
-        '回复单位':[None if replyer=='' else replyer.split('\xa0')[-1],0],
-        '回复日期':[replydate,0],
-        '来信内容':[content,0],
-        '回复内容':[replycontent,0],
-        'link':[url,0],
-        'id':[id,0]
-         }
-        return data
-#统计已获取列表
-data1={
-            '标题':['标题','标题'],
-            '来信日期':['标题','标题'],
-            '回复单位':['标题','标题'],
-            '回复日期':['标题','标题'],
-            '来信内容':['标题','标题'],
-            '回复内容':['标题','标题'],
-            'link':['标题','标题'],
-            'id':['标题','标题']
-        }
-df_get=pd.DataFrame(data1,columns=['标题','来信日期','回复单位','回复日期','来信内容','回复内容','link','id'])
-df_get=df_get.drop(1)
-df_get=df_get.drop(0)
-
-path='C:/Users/Administrator/Desktop/董事长信箱解答/董事长信箱记录导出'
-df_previous=pd.read_csv(path+"/董事长信箱.csv", encoding='gbk')
-driver = webdriver.PhantomJS(executable_path=path+"/phantomjs-2.1.1-windows/bin/phantomjs.exe")
-#num1=input('请输入最新的信箱编码：')
-#num2=input('请输入要查找信件数量：')
-df=df_previous['id']
-for i in df.index:
-    id=df[i]
-    print(i,id)
-    data=get_info(int(id))
-    if data:
-        frame=pd.DataFrame(data,columns=['标题','来信日期','回复单位','回复日期','来信内容','回复内容','link','id'])
-        frame=frame.drop(1)
-        df_get=df_get.append(frame)
-
+        df_get.loc[len(df_get)+1]=[title,date,None if replyer=='' else replyer.split('\xa0')[-1],replydate,content,replycontent,url,id]
 driver.quit()
-#df_main=df_get.append(df_previous)
-#df_main.drop_duplicates()
-df_get.to_csv(path+"/董事长信箱1.csv",indes=False)
-
-
-
+df_get.to_csv(path+"/董事长信箱1.csv",index=False)
